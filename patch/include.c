4deaea94f956dda7ae3ae767151fe998c3297884
flohh-py <jbmflohh@gmail.com>
2022-05-06 18:01:18 -0400

    initial commit

---
+++ patch/include.c
@@ -0,0 +1,24 @@
+/* Bar functionality */
+#include "bar_indicators.c"
+#include "bar_tagicons.c"
+
+#include "bar_ltsymbol.c"
+#include "bar_status.c"
+#include "bar_statuscolors.c"
+#include "bar_tags.c"
+#include "bar_wintitle.c"
+#include "bar_awesomebar.c"
+#include "bar_systray.c"
+#include "bar_wintitleactions.c"
+
+/* Other patches */
+#include "autostart.c"
+#include "cyclelayouts.c"
+#include "rotatestack.c"
+#include "shiftview.c"
+#include "vanitygaps.c"
+#include "warp.c"
+/* Layouts */
+#include "layout_facts.c"
+#include "layout_tile.c"
+
---
+++ patch/include.h
@@ -0,0 +1,23 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_statuscolors.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_awesomebar.h"
+#include "bar_systray.h"
+#include "bar_wintitleactions.h"
+
+/* Other patches */
+#include "autostart.h"
+#include "cyclelayouts.h"
+#include "rotatestack.h"
+#include "shiftview.h"
+#include "vanitygaps.h"
+#include "warp.h"
+/* Layouts */
+#include "layout_tile.h"
+
